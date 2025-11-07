590bd08e0f3b9424a8a7c3144ce911a56a6b3cf0
John Gomersall <thegoms@gmail.com>
2025-11-07 17:11:46 +0000

    test: More tests
    Signed-off-by: John Gomersall <thegoms@gmail.com>

--- tests/metadata/metadata_test.py
+++ tests/metadata/metadata_test.py
@@ -5,12 +5,26 @@ from jsonschema import ValidationError, validate
 
 script_dir = os.path.dirname(__file__)
 
+def relative_path(file_name):
+    return os.path.join(script_dir,file_name)
+
+def load_json(file_name):
+    with open(relative_path(file_name)) as f:
+        json_data = json.load(f)
+    return json_data
+    
 class MetadataTests(unittest.TestCase):
     def test_metadata(self):
-        with open(os.path.join(script_dir,'../../schemas/metadata.json')) as f:
-            schema = json.load(f)
+        schema = load_json('../../schemas/metadata.json')
 
-        with open(os.path.join(script_dir,'invalid/root_array.json')) as f:
-            invalid = json.load(f)
+        for invalid_file in sorted(os.listdir(relative_path('invalid'))):
+            invalid_json = load_json(os.path.join('invalid', invalid_file))
+            with self.assertRaises(ValidationError, msg=invalid_file):
+                validate(invalid_json, schema)
 
-        self.assertRaises(ValidationError, validate, invalid, schema)
+        for valid_file in sorted(os.listdir(relative_path('valid'))):
+            valid_json = load_json(os.path.join('valid', valid_file))
+            try:
+                validate(valid_json, schema)
+            except ValidationError as e:
+                self.fail(f"Valid file: {valid_file} raised {e.message}")
